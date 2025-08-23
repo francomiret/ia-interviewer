01e04fa60d1cd237ccbe70560f35ff254a633792
Franco Miret <francomiret11@gmail.com>
2025-08-23 17:19:05 -0300

    feat: update styles in page.tsx

--- interviewer/app/(root)/page.tsx
+++ interviewer/app/(root)/page.tsx
@@ -2,7 +2,7 @@ import { Contacts, HeroBlock, HowItWorks, Navbar, Pricing } from "./components";
 
 export default function Home() {
   return (
-    <div className="min-h-screen w-full bg-gradient-to-br from-slate-900 via-blue-900 to-slate-900">
+    <div className="min-h-screen w-full bg-gradient-to-b from-blue-900 via-blue-800 to-blue-900">
       <Navbar />
       <HeroBlock />
       <HowItWorks />
